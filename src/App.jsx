ff274b7217802209785c23015de13ead071e7fbc
Luis <garcia.luis.enrique19@gmail.com>
2023-06-14 23:05:51 -0700

    change some colors and add another css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import { useState } from 'react'
 import { puppyList } from './data';
 import './App.css'
+import './style.css'
+
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
 
-  console.log("puppyList: ", puppyList);
+ 
 
   return (
+    
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+            
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+            
+            
+          </p>
+        );
+      })}      
+      {featPupId && (
+        <div className="detailsDiv">
+          <h2><p className='nameTag'>{featuredPup.name}</p></h2>
+          <ul>
+            <li><p className='ageTag'>Age: {featuredPup.age}</p></li>
+            <li><p className='emailTag'>Email: {featuredPup.email}</p></li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
+
+
+
 
 export default App
